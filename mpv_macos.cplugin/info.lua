fc2ebf0b12dc8648d04f2e3918e35cf16a49f13a
isaac <cissusnar@gmail.com>
2019-03-17 18:51:15 +0800

    增加MPV打开URL-macOS

---
+++ mpv_macos.cplugin/info.lua
@@ -0,0 +1,6 @@
+plugin_name = "mpv打开URL"
+plugin_bundle_id = "com.developer.793D81C4"
+plugin_desc = "使用mpv打开URL"
+plugin_version = "1.0"
+plugin_author = "http://clipber.com"
+cpkgen_version = "1.0"
---
+++ mpv_macos.cplugin/main.lua
@@ -0,0 +1,18 @@
+local app = require("app")
+
+function urlencode(str)
+	if (str) then
+		str = string.gsub (str, "\n", "\r\n")
+		str = string.gsub (str, "([^%w ])",
+		function (c) return string.format ("%%%02X", string.byte(c)) end)
+		str = string.gsub (str, " ", "+")
+	end
+	return str
+end
+
+function main(text)
+	-- edit here
+	local url = "mpv://" .. urlencode(text)
+	local txt = app.openURL(url);
+	return txt
+end
