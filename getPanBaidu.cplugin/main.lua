3d43c46e43c8d86ac29d1a1aa32ca29894103385
cissusnar <cissusnar@gmail.com>
2021-07-03 01:35:42 +0800

    fix nil return

--- getPanBaidu.cplugin/main.lua
+++ getPanBaidu.cplugin/main.lua
@@ -1,6 +1,6 @@
 function main(text)
 	 i = string.find(text, "pan.baidu.com")
-    if i == nil then return end
+    if i == nil then return "" end
 	_,_,str=string.find(text,"提取码.-(%w%w%w%w)");
 	return str
 end
